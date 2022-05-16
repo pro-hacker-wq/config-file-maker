6cf454df0d684649cc60eced2b5205f1a29c460f
pro-hacker-wq <104883936+pro-hacker-wq@users.noreply.github.com>
2022-05-16 21:27:24 +0530

    Python config 4
    Python config

---
+++ helper.py
@@ -0,0 +1,13 @@
+import configparser 
+
+  
+
+ # Method to read config file settings 
+
+ def read_config(): 
+
+     config = configparser.ConfigParser() 
+
+     config.read('configurations.ini') 
+
+     return config
